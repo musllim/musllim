ebf0a0cd2b49585ddc2351c0c53eb3008939f2fb
UWITONDANISHEMA Muslim <muslimuwitondanishema@gmail.com>
2023-06-23 09:51:18 +0300

    Delete a.py

--- a.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-with open('lang-and-tools.svg', 'rb') as file:
-    svg_data = file.read()
-
-base64_encoded_data = base64.b64encode(svg_data).decode('utf-8')
-
-print(f"![SVG Image](data:image/svg+xml;base64,{base64_encoded_data})")
